e0b6c3ced78046cdfa9d1461a8aa24a58a978f52
nirkbr <139012728+nirkbr@users.noreply.github.com>
2024-04-11 17:27:30 -0300

    Problema Resolvido.
    O problema anterior se encontrava na tag que utilizava para chamar as imagens, antes eu utilizava o nome Imagens para tentar chamar a pasta de mesmo nome porém não dava resultado, então, com auxílio de outros programadores mais experientes troque o nome de Imagens para  img o que resolveu o problema das imagens porém descobri outro logo em seguida. 
    As imagens das mulheres não estavam aparecendo e então fui resolver este problema que, se encontrava na lógica que utilizei no IF.
    Basicamente if puxava primeiro as faixas etárias ao invés de primeiro puxar o gênero selecionado o que fazia com que sempre que o usuário escolhesse a o gênero feminino não chamasse as imagens então dei uma pesquisada e descobri o problema logo o resolvendo.

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
 function verificar() {
-let data = new Date()
-let ano = data.getFullYear()
-let forAno = document.getElementById('txtano')
-let resp = window.document.querySelector('div#resp')
+    let data = new Date()
+    let ano = data.getFullYear()
+    let forAno = document.getElementById('txtano')
+    let resp = window.document.querySelector('div#resp')
 
-if (forAno.value.length == 0 || Number(forAno.value) > ano){
-    window.alert('[ERRO] por favor verifique as informações inserida!')
-} else{
-        let fsex = document.getElementsByName('radsex')
-        let idade = ano - Number(forAno.value)
-        let gênero = ''
-        let img = document.createElement('Imagens')
-        img.setAttribute('id', 'Imagens')
-        if (fsex[0].checked) {
-            gênero = 'Homem';
-        if (idade >= 0 && idade < 10){
-            //criança
-            img.setAttribute('src', 'Imagens/Menino.jpg')
-        }else if(idade < 22){
-            //Jovem
-            img.setAttribute('src', 'Imagens/Homem.jpg')
-        }else if (idade < 60){
-            //Adulto
-            img.setAttribute('src', 'Imagens/HomemAdulto.jpg')
+    if (forAno.value.length == 0 || Number(forAno.value) > ano){
+        window.alert('[ERRO] por favor verifique as informações inserida!')
+    } else{
+            let fsex = document.getElementsByName('radsex')
+            let idade = ano - Number(forAno.value)
+            let gênero = ''
+            let img = document.createElement('img')
+            img.setAttribute('id', 'Imagens')
+            if (fsex[0].checked) {
+                gênero = 'Homem';
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'Imagens/Menino.jpg')
+            }else if(idade < 22){
+                //Jovem
+                img.setAttribute('src', 'Imagens/Homem.jpg')
+            }else if (idade < 60){
+                //Adulto
+                img.setAttribute('src', 'Imagens/HomemAdulto.jpg')
+            }else{
+                //Idoso
+                img.setAttribute('src', 'Imagens/Senhoridoso.jpg')
+            }
+            } else if(fsex[1].checked){
+                gênero = 'Mulher';
+                if(idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'Imagens/Menina.jpg')
+            }else if(idade < 21){
+                //Jovem
+                img.setAttribute('src', 'Imagens/Mulher.jpg')
+            }else if(idade < 60){
+                //Adulto
+                img.setAttribute('src', 'Imagens/MulherAdulta.jpg')
+            }else{
+                //idoso
+                img.setAttribute('src', 'Imagens/senhoraidosa.jpg')
+            } 
         }else{
-            //Idoso
-            img.setAttribute('src', 'Imagens/Senhoridoso.jpg')
-        }
-        } else if(fsex[1].checked){
-            gênero = 'Mulher'
-        }
-        else if(idade >= 0 && idade < 10){
-            //criança
-            img.setAttribute('src', 'Imagens/Menina.jpg')
-        }else if(idade < 21){
-            //Jovem
-            img.setAttribute('src', 'Imagens/Mulher.jpg')
-        }else if(idade < 60){
-            //Adulto
-            img.setAttribute('src', 'Imagens/MulherAdulta.jpg')
-        }else {
-            //idoso
-            img.setAttribute('src', 'Imagens/Senhoradoso.jpg')
-        }
+            alert("[ERRO] PArece que você não selecionou um dos Genêros. Por favor selecione um deles.")
+            }
 
-        resp.style.textAlign = 'center';
-        resp.innerHTML = `Detectamos ${gênero} com ${idade} anos`;
-        resp.appendChild(img);
-    } 
-}
+            resp.style.textAlign = 'center';
+            resp.innerHTML = `Detectamos ${gênero} com ${idade} anos`;
+            resp.appendChild(img);
+        } 
+    }
